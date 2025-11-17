8042f642c1313664b88cffc9b47178a525815c14
udayugade <udayugade10@gmail.com>
2025-11-17 17:57:37 +0530

    Add files via upload

---
+++ ass1.py
@@ -0,0 +1,25 @@
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("You entered:", a, "and", b)
+
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+
+if b != 0:
+    print("Division:", a / b)
+else:
+    print("Division: Not possible (division by zero)")
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+full_name = first_name + " " + last_name
+
+print("Hello", full_name + "!", "Welcome!")
